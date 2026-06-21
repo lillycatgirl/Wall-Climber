97c847c4749ad34e7714959a970d22eec99749f7
lillycatt <lillycatgirl@proton.me>
2026-06-21 14:58:32 +1200

    Update Hand Controller.cs
    Raycast to not allow midair grabbing

--- Hand Controller.cs
+++ Hand Controller.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class HandController : MonoBehaviour
 {
@@ -9,9 +11,8 @@ public class HandController : MonoBehaviour
 
     [SerializeField] private float springDistanceTarget;
     [SerializeField] private float springDistanceLerp;
-
+    [SerializeField] private LayerMask wallsLayerMask;
     [SerializeField] private int mouseButton;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _cam = Camera.main;
@@ -19,19 +20,22 @@ public class HandController : MonoBehaviour
         _spring = GetComponent<SpringJoint2D>();
     }
 
-    // Update is called once per frame
     private void FixedUpdate()
     {
-        _spring.enabled = true;
-        
         _spring.distance = Mathf.Lerp(_spring.distance, springDistanceTarget, Mathf.Exp(springDistanceLerp));
 
         if (Input.GetMouseButton(mouseButton))
         {
+            // Shoot a raycast from player origin to test for wall to grab onto
             var mousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
             mousePos.z = 0;
-            _rb.MovePosition(mousePos);
-            _spring.distance = Vector2.Distance(_spring.connectedBody.position,_rb.transform.position);
+            var ray = Physics2D.Raycast(_spring.connectedBody.position, (Vector2)mousePos -  _spring.connectedBody.position, Mathf.Infinity, wallsLayerMask);
+            if (ray.collider != null)
+            {
+                print(ray.collider.gameObject.name);
+                _rb.MovePosition(ray.point);
+                _spring.distance = Vector2.Distance(_spring.connectedBody.position,_rb.transform.position);
+            }
         }
         
     }
